6122670d0fc5dad1ddae9b4653fc4fa4602a539b
Leonardo Aldana <112524903+compuleo@users.noreply.github.com>
2024-10-09 06:20:31 -0500

    primeros ejercicios

---
+++ app.js
@@ -0,0 +1,38 @@
+
+//
+/*
+let elemento = document;
+elemento = document.body;
+console.log(elemento);
+
+let elementoW = window;
+console.log(`ELEMENTOS DE WINDOW $(elementoW)`);
+*/
+
+/*
+let elementoE = document.styleSheets;
+console.log(elementoE)
+*/
+
+/*
+let elementoId = document.getElementById('tituloDOM');
+console.log(elementoId);
+
+let elementoDom = document.getElementById('contenidoDOM');
+console.log('contenidoDom');
+*/
+
+/*
+let opId = document.getElementById('p1');
+opId.innerHTML = 'JavaScript';
+
+let opId2 = document.getElementById('p2');
+opId2.innerHTML = 'Cru cru cantaba la rana';
+*/
+
+/*
+let elementoClase = document.getElementsByClassName('clase');
+console.log(elementoClase);
+*/
+
+
